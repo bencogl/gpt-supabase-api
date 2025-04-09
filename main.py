1fabad90fecd320034b9774ee00932e630904d27
bencogl <ben.coglitore.srg@gmail.com>
2025-04-09 15:53:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from supabase import create_client
 import fitz  # PyMuPDF
 import tempfile
@@ -9,6 +10,7 @@ SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJ
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = Flask(__name__)
+CORS(app)  #ABILITA CORS UNIVERSALE
 BUCKET = "benchbila"
 TABELLA = "tabella"
 
